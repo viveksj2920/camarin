b7bcbd09de07c593431356619852f7a53120bda0
vivek <161866010+vivekatneucleusx@users.noreply.github.com>
2025-03-31 02:18:24 +0530

    fixed moderator

--- comments/moderation.py
+++ comments/moderation.py
@@ -1,7 +1,12 @@
 from google.cloud import language_v1
+from google.oauth2 import service_account
+import os
 
 def moderate_text(content: str) -> tuple[bool, str]:
-    client = language_v1.LanguageServiceClient()
+    credentials = service_account.Credentials.from_service_account_file(
+    os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+)
+    client = language_v1.LanguageServiceClient(credentials=credentials)
 
     document = language_v1.Document(content=content, type_=language_v1.Document.Type.PLAIN_TEXT)
 
